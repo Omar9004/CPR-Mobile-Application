2085b163f994661fd0380e2ca7f206413dc16390
Omar Alfakir <omarfakeer@gmail.com>
2021-04-21 13:49:20 +0200

    04/21

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import React, { Component, useState,useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { Alert, StyleSheet,TouchableOpacity, Text, View, Touchable,Button, TextInput } from 'react-native';
 
-import Screen2 from './Screens/Screen2';
-import Screen1 from './Screens/Screen1';
+import Start from './Screens/Start';
 import Options from './Screens/Options';
 import VF_VT from './VF_VT/VF_VT';
 import  VF_VT2 from './VF_VT/VF_VT2';
 import CPR_Start from './Screens/CPR_Start';
-
+import Asystoli from './Screens/Asystoli';
 import {createAppContainer}from 'react-navigation';
 import {createStackNavigator}from 'react-navigation-stack';
 import {createSwitchNavigator}from 'react-navigation';
@@ -32,32 +31,55 @@ const AppSwitherVF = createSwitchNavigator({
   },
   VF_VT2:{
     screen:VF_VT2
-  }
-},{
-  initialRouteName: "VF_VT"
-})
-const AppNavigator = createStackNavigator({
-  Home:{
-    screen:Screen2,
-    
+  },
+  Asystoli:{
+    screen:Asystoli,
   },
   Options:{ 
-    screen:Options
+    screen:Options,
   },
+  
+},
+{
+  initialRouteName: "Options",
+  defaultNavigationOptions: {
+    gestureEnabled:false,
+    headerStyle: {
+      backgroundColor: '#8ed1fc',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+    
+  }
+
+})
+/*const AppSwitherAS = createSwitchNavigator({
+    Asystoli:{
+    screen:Asystoli,
+  },
+})*/
+const AppNavigator = createStackNavigator({
+  Home:{
+    screen:Start,
+    
+  },
+
   CPR_Start:{
     screen:CPR_Start
   },
 
-  Screen1:{
-    screen:Screen1
-  },
-  VF:AppSwitherVF
+  
+  VF:AppSwitherVF,
+ // Asystoli:AppSwitherAS,
   
     
 },
 {
   initialRouteName: "Home",
   defaultNavigationOptions: {
+    gestureEnabled:false,
     headerStyle: {
       backgroundColor: '#8ed1fc',
     },
